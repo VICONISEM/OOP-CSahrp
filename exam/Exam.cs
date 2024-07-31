7e9a78d3c9a8128de602ef94d81e92b7bc866f07
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-08-01 01:39:04 +0300

    make Exam with two types and abstracted Methods

---
+++ exam/Exam.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using question;
+
+namespace OOP_Exam.Exam
+{
+    public abstract class Exam
+    {
+        private int timeOfExam;
+
+        public int TimeOfExam
+        {
+            get { return timeOfExam; }
+            set { timeOfExam = value; }
+        }
+
+        private int numberOfQuestion;
+        
+        public int NumberOfQuestion
+        {
+            get { return numberOfQuestion; }
+            set { numberOfQuestion = value;}
+        }
+
+
+        public abstract void ShowExam();
+        public abstract void MakeExam();
+
+
+
+    }
+}
